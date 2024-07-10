d0d376df272aa57016de513fb4912818f89dcdff
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-07-10 16:21:40 +0100

    Removed unnecessary code ffrom call_gpt and created basic generate file

--- QCLM-Bench/generation/generate.py
+++ QCLM-Bench/generation/generate.py
@@ -1,2 +1,9 @@
+from utils import call_gpt, call_mimic
+
 def main():
-    print('generating')
+    discharge_summary = call_mimic()
+    question_answer_pair = call_gpt(discharge_summary)
+    print(question_answer_pair)
+
+if __name__ == "main":
+    main()
--- QCLM-Bench/utils/call_gpt.py
+++ QCLM-Bench/utils/call_gpt.py
@@ -3,42 +3,39 @@
 # Prompt model to generate a question and answer based on the context
 # with specifications and requirements for the type of question
 
-# %%
 import os
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = AzureOpenAI(
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), 
-    api_key=os.getenv("AZURE_OPENAI_KEY"),
-    api_version=os.getenv("AZURE_API_VERSION")
-)
+def call_gpt(discharge_summary):
 
-model_name = "gpt-35-turbo-16k"
+    client = AzureOpenAI(
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), 
+        api_key=os.getenv("AZURE_OPENAI_KEY"),
+        api_version=os.getenv("AZURE_API_VERSION")
+    )
 
+    model_name = "gpt-35-turbo-16k"
 
-patient_note = "note"
-question = "tell me about blah"
+    question = "Please create a question-answer pair out of this discharge summary to benchmark the capabilities of a question-answering large language model"
 
+    prompt = [{
+        "role": "user",
+        "content": (
+                "Discharge Summary :\n"
+                f"{patient_note}\n\n"
+                f"Request : {question}\n\n"
+                "Answer :"
+            )
+    }]
 
-prompt = [{
-    "role": "user",
-    "content": (
-			"Discharge Summary :\n"
-			f"{patient_note}\n\n"
-			f"Request : {question}\n\n"
-			"Answer :"
-        )
-}]
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=prompt,
+        max_tokens=600,
+        temperature=0
+    )
 
-
-response = client.chat.completions.create(
-    model=model_name,
-    messages=prompt,
-    max_tokens=600,
-    temperature=0
-)
-
-print(response.to_json())
+    return response.to_json()
