0f672c51ca468937a72f792167e3fad830ec75f2
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-07-10 11:58:59 +0100

    Updated gitignore and commented call_mimic.py

--- QCLM-Bench/utils/call_mimic.py
+++ QCLM-Bench/utils/call_mimic.py
@@ -8,9 +8,9 @@ database_password = os.environ.get('DATABASE_PASSWORD')
 database_name = os.environ.get('DATABASE_NAME')
 database_port = os.environ.get('DATABASE_PORT')
 
-# Set up DB connection
 def call_mimic():
     
+    # Set up DB connection
     connection = psycopg2.connect(
         host=database_host,
         user=database_username,
