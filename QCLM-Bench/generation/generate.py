f27324a148ef9155bef7f0166057bc308438985b
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-07-11 10:32:00 +0100

    Updated imports on generate.py

--- QCLM-Bench/generation/generate.py
+++ QCLM-Bench/generation/generate.py
@@ -1,6 +1,11 @@
+import sys
+import os
 
-from ..utils.call_gpt import call_gpt
-from ..utils.call_mimic import call_mimic
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.insert(0, parent_dir)
+
+from utils.call_gpt import call_gpt
+from utils.call_mimic import call_mimic
 
 def main():
     discharge_summary = call_mimic()
