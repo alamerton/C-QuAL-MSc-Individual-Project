1f9df5de877d3973c3621acff6f1c7dcd4904319
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-07-11 13:27:50 +0100

    Updated file paths to adhere to new directory structure

--- generation/generate.py
+++ generation/generate.py
@@ -3,16 +3,14 @@ import sys
 import os
 import time
 from tqdm import tqdm
+import pandas as pd
 
-parent_dir = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), os.pardir)
-)
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_dir)
 from utils.call_gpt import call_gpt
 from utils.call_mimic import get_summaries_for_generation
-import pandas as pd
 
-NUMBER_OF_QA_PAIRS = 2
+NUMBER_OF_QA_PAIRS = 10
 SUMMARIES_DESTINATION = "function"
 
 def main():
@@ -40,19 +38,19 @@ def main():
         )
 
         # Parse the json to get the question and answer as variables
-        data_item.append(question_answer_string.split('\n'))
+        data_item.extend(question_answer_string.split('\n'))
 
         # Add Q-A pair to dataframe
         data.loc[row] = data_item
 
         print(f"{row+1}/{NUMBER_OF_QA_PAIRS}")
-        time.sleep(10) 
+        # time.sleep(10) 
     
     print("Complete")
     print(data)
 
     # Write dataset to output directory
-    output_path = f'QCLM-Bench/data/{NUMBER_OF_QA_PAIRS}-QA-pairs-{date}'
+    output_path = f'data/{NUMBER_OF_QA_PAIRS}-QA-pairs-{date}'
     data.to_csv(f"{output_path}.csv")
     print("Dataset saved")
 
