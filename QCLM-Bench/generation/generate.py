76e51deadb9e5943cb3350c050845b3eac2604aa
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-07-11 10:48:31 +0100

    Successfully generating QA pair with MIMIC-III

--- QCLM-Bench/generation/generate.py
+++ QCLM-Bench/generation/generate.py
@@ -1,16 +1,19 @@
 import sys
 import os
-
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.insert(0, parent_dir)
-
 from utils.call_gpt import call_gpt
 from utils.call_mimic import call_mimic
 
 def main():
     discharge_summary = call_mimic()
-    question_answer_pair = call_gpt.call_gpt(discharge_summary)
+    print(discharge_summary)
+    print("hello world")
+    question_answer_pair = call_gpt(discharge_summary)
     print(question_answer_pair)
+    return question_answer_pair
+    # Probably add for loop for generating x qa pairs
+    # Probably write dataset to output directory
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
