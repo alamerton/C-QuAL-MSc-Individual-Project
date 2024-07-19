85d87116d6a3b57bacc262b2b19cbfd023f936b7
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-07-19 11:18:04 +0100

    Added pseudocode and commented generate.py

--- evals/QA_annotations.py
+++ evals/QA_annotations.py
@@ -7,4 +7,12 @@ suggested revision on the row.
 A copy of the annotated and edited dataset should then be saved, and 
 a copy of the resulting dataset should have the annotations removed, 
 and be saved as a finished QA dataset.
+
+    1. Load the QA dataset, and create a new column called 
+    '[model_name]_annotation'
+    2. For every row in the dataset:
+        2.1. Call the LLM with the row and prompt
+        2.2. Save the annotation to the annotation column
+    3. Save the dataset somewhere e.g. locally or HF
+
 """
