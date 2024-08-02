252bcaa778d208baa0c1fa2bcdce60f77fcf5274
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-08-02 13:55:56 +0100

    Updated record_model_answers to save the model response in one row and column

--- evals/benchmark.py
+++ evals/benchmark.py
@@ -4,7 +4,8 @@ import sys
 import os
 from sklearn.metrics import f1_score
 import nltk
-from sentence_transformers import SentenceTransformer, util
+import tensorflow_hub as hub
+import numpy as np
 from rouge import Rouge
 from nltk.translate.bleu_score import sentence_bleu
 import spacy
@@ -18,35 +19,24 @@ DATASET_PATH = "data/generations/3-QA-pairs-2024-08-01 14:04:41.574896.csv"
 MODEL_NAME = "gpt-35-turbo-16k"
 
 nltk.download("punkt")
-sas_model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 rouge = Rouge()
-nlp = spacy.load("en_core_sci_md")
-nlp.add_pipe("scispacy_linker", config={"resolve_abbreviations": True, "linker_name": "umls"})
-
-
 
 
 def record_model_answers(dataset_path, model_name):
     # TODO: extend to include local models
-
     print("Loading dataset")
+    dataset = pd.read_csv(dataset_path)
 
-    dataset: pd.DataFrame = pd.read_csv(dataset_path)
-
-    for _, row in tqdm(
+    for index, row in tqdm(
         dataset.iterrows(), total=len(dataset), desc=f"Benchmarking model"
     ):
-
         discharge_summary = row["Discharge Summary"]
         question = row["Question"]
         response = benchmark_with_azure(model_name, discharge_summary, question)
-        dataset[f"{model_name} Response"] = response
 
+        dataset.at[index, f"{model_name} Response"] = response
     return dataset
-
-
-# some of these might need the discharge summay, and some might not need the
-# expected response.
 
 
 def get_exact_match(expected_answer: str, model_answer: str):
@@ -61,10 +51,9 @@ def get_f1_score(expected_answer: str, model_answer: str):
 
 
 def get_sas(expected_answer: str, model_answer: str):
-    expected_answer = sas_model.encode(expected_answer, convert_to_tensor=True)
-    model_answer = sas_model.encode(model_answer, convert_to_tensor=True)
-    similarity = util.pytorch_cos_sim(expected_answer, model_answer)
-    return similarity.item()
+    embeddings = embed([expected_answer, model_answer])
+    similarity_matrix = np.inner(embeddings, embeddings)
+    return similarity_matrix[0, 1]
 
 
 def get_rouge(expected_answer: str, model_answer: str):
@@ -77,12 +66,10 @@ def get_bleu(expected_answer: str, model_answer: str):
     return sentence_bleu(expected_tokens, model_tokens)
 
 
-def get_clinical_concept_extraction(model_answer: str):
-    doc = nlp(model_answer)
-    return [e._.kb_ents for e in doc.ents]
+def get_cce(model_answer: str):
+    return 0
 
-
-def get_medical_relation_extraction(expected_answer: str, model_answer: str):
+def get_mre(expected_answer: str, model_answer: str):
     return 0
 
 
@@ -91,10 +78,7 @@ def get_g_eval(expected_answer: str, model_answer: str):
 
 
 def score_model(dataset):
-
-    # loop through each row in the dataset:
-    # save exact match scores to arrays
-
+    
     (
         em_scores,
         f1_scores,
@@ -106,7 +90,7 @@ def score_model(dataset):
         g_eval_scores,
     ) = []
 
-    for row in range(0, len(dataset)):
+    for row in tqdm(range(0, len(dataset))):
         expected_answer = dataset["Expected Answer"][row]
         model_answer = dataset["Model Answer"][row]
 
@@ -115,9 +99,18 @@ def score_model(dataset):
         sas_scores.append(get_sas(expected_answer, model_answer))
         rouge_scores.append(get_rouge(expected_answer, model_answer))
         bleu_scores.append(get_bleu(expected_answer, model_answer))
+        cce_scores.append(get_cce(expected_answer, model_answer))
+        mre_scores.append(get_mre(expected_answer, model_answer))
+        g_eval_scores.append(get_g_eval(expected_answer, model_answer))
 
-
-    # get average of arrays to get final scores
+    exact_match = sum(em_scores) / len(em_scores)
+    f1 = sum(f1_scores) / len(f1_scores)
+    semantic_answer_similarity = sum(sas_scores) / len(sas_scores)
+    rouge = sum(rouge_scores) / len(rouge_scores)
+    bleu = sum(bleu_scores) / len(bleu_scores)
+    clinical_concept_extraction = sum(cce_scores) / len(cce_scores)
+    medical_relation_extraction = sum(mre_scores) / len(mre_scores)
+    g_eval = sum(g_eval_scores) / len(g_eval_scores)
 
     benchmarking_results = pd.DataFrame(
         {
@@ -131,8 +124,15 @@ def score_model(dataset):
                 "Medical Relation Extraction",
                 "G-Eval",
             ],
-            "Value": [
+            f"{MODEL_NAME} Score": [
                 exact_match,
+                f1,
+                semantic_answer_similarity,
+                rouge,
+                bleu,
+                clinical_concept_extraction,
+                medical_relation_extraction,
+                g_eval,
             ],
         }
     )
@@ -140,12 +140,14 @@ def score_model(dataset):
 
 
 def main():
-    # model_answers = record_model_answers(DATASET_PATH, MODEL_NAME)
-    # save_dataset(model_answers, directory="model-answers")
-    # benchmarking_results = score_model(model_answers)
-    # save_dataset(benchmarking_results, directory="benchmarking-results")
-    clinical_string = "hello my chest hurts from pain in the heart cancer"
-    print(get_clinical_concept_extraction(clinical_string))
+    model_answers = record_model_answers(DATASET_PATH, MODEL_NAME)
+    save_dataset(model_answers, directory="model-answers")
+    benchmarking_results = score_model(model_answers)
+    save_dataset(benchmarking_results, directory="benchmarking-results")
+
+    # clinical_string = "hello my chest hurts from pain in the heart cancer"
+    # print(get_cce(clinical_string))
+
 
 if __name__ == "__main__":
     main()
