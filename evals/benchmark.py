fa38bb9a9d83ea4613d0e9cca952ab2952a36df8
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-08-02 11:12:19 +0100

    Updated save_dataset to no longer include a local tag, might add a save_to_hugging_face function separately, but for now the flag is obsolete. Also added exact_match to benchmark file

--- evals/benchmark.py
+++ evals/benchmark.py
@@ -1,20 +1,17 @@
-"""
-This file should load a QA dataset and model, and loop through the dataset,
-prompting the model with each discharge summary and question, saving
-the answer to a copy of the dataset with a model answer column.
-"""
 import pandas as pd
 from tqdm import tqdm
-import sys, os
+import sys
+import os
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, parent_dir)
 from utils.evals.benchmark_with_azure import benchmark_with_azure
 from utils.misc import save_dataset
 
-DATASET_PATH = 'data/generations/10-QA-pairs-2024-07-17 15:48:59.369671.csv'
-CLOUD = True
+DATASET_PATH = "data/generations/10-QA-pairs-2024-07-17 15:48:59.369671.csv"
+# CLOUD = True
 MODEL_NAME = "gpt-35-turbo-16k"
+
 
 def record_model_responses(dataset_path, model_name):
     # TODO: extend to include local models
@@ -24,67 +21,78 @@ def record_model_responses(dataset_path, model_name):
     dataset: pd.DataFrame = pd.read_csv(dataset_path)
 
     for _, row in tqdm(
-        dataset.iterrows(),
-        total = len(dataset),
-        desc = f'Benchmarking model'
+        dataset.iterrows(), total=len(dataset), desc=f"Benchmarking model"
     ):
-        
-        discharge_summary = row['Discharge Summary']
-        question = row['Question']
-        response = benchmark_with_azure(
-            model_name,
-            discharge_summary,
-            question
-        )
-        dataset[f'{model_name} Response'] = response
-    
+
+        discharge_summary = row["Discharge Summary"]
+        question = row["Question"]
+        response = benchmark_with_azure(model_name, discharge_summary, question)
+        dataset[f"{model_name} Response"] = response
+
     return dataset
+
 
 # some of these might need the discharge summay, and some might not need the
 # expected response.
 
+
 def get_exact_match(expected_response: str, model_response: str):
-    return 0
+    return expected_response == model_response
+
 
 def get_f1_score(expected_response: str, model_response: str):
     return 0
 
+
 def get_semantic_answer_similarity(expected_response: str, model_response: str):
     return 0
+
 
 def get_rouge(expected_response: str, model_response: str):
     return 0
 
+
 def get_bleu(expected_response: str, model_response: str):
     return 0
 
+
 def get_clinical_concept_extraction(expected_response: str, model_response: str):
-    # might not need 
+    # might not need
     return 0
+
 
 def get_medical_relation_extraction(expected_response: str, model_response: str):
     return 0
 
+
 def get_g_eval(expected_response: str, model_response: str):
     return 0
 
+
 def score_model(dataset):
-    #TODO: write this function. 
-    """Score the model  on how close its answer is to the expected 
-    answer. Scoring metrics:
-    - Exact match
-    - F1 Score
-    - Semantic answer similarity
-    - ROUGE
-    - Clinical concept extraction
-    - Medical relation extraction
-    - Semantic textual similarity
-    - Natural language inference (NLI)
-    - Medical question answering (MQA)
-    - Safety evaluations
-    """
-    
+    exact_match = get_exact_match()
+    benchmarking_results = pd.DataFrame(
+        {
+            "Metric": [
+                "Exact Match",
+                "F1 Score",
+                "Semantic Answer Similarity",
+                "Rouge",
+                "BLEU",
+                "Clinical Concept Extraction",
+                "Medical Relation Extraction",
+                "G-Eval",
+            ],
+            "Value": [
+                exact_match,
+            ],
+        }s
+    )
+    return benchmarking_results
+
 
 def main():
     model_responses = record_model_responses(DATASET_PATH, MODEL_NAME)
-    save_dataset(model_responses, local=True) #TODO: pass argument to save in model responses directory in data
+    save_dataset(model_responses, directory="model_responses")
+    benchmarking_results = score_model(model_responses)
+    save_dataset(benchmarking_results, directory="benchmarking-results")
--- utils/misc.py
+++ utils/misc.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-def save_dataset(dataset, local: bool):
+def save_dataset(dataset, directory: str):
     date = datetime.now()
     rows = len(dataset)
     # TODO: add model name to file name
-    output_path = f'data/annotations/{rows}-QA-pairs-{date}'
+    output_path = f'data/{directory}/{rows}-QA-pairs-{date}'
     dataset.to_csv(f"{output_path}.csv")
