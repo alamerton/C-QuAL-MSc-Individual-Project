14633e99a3763b756e9dd48fdf11cad9a9edad61
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-08-05 16:44:59 +0100

    Restructured function

--- evals/process.py
+++ evals/process.py
@@ -3,11 +3,11 @@ import pandas as pd
 import re
 
 DATASET_PATH = '../data/generations/c-qual-xl.csv'
+SAVE_PATH = '../data/processing/matching_pairs/dataset_processed_overwrite.csv's
 
 # Open a dataset and remove any row where the answer contains an 
 # exact match of a given length
 def remove_n_matching_pairs(question, answer, n):
-
     question = str(question) if pd.notna(question) else ''
     answer = str(answer) if pd.notna(answer) else ''
 
@@ -20,11 +20,13 @@ def remove_n_matching_pairs(question, answer, n):
                 return True
     return False
 
-def removed_spurious_pairs(dataset_path):
+def remove_spurious_pairs(dataset_path, save_path):
     df = pd.read_csv(dataset_path)
     df_filtered = df[~df.apply(
         lambda row: remove_n_matching_pairs(
             row['Question'], row['Expected Answer']), 3, axis=1)
         ]
+    df_filtered.to_csv(save_path)
     print("New shape: ", df_filtered.shape)
-    df_filtered.to_csv(f'../data/processing/matching_pairs/dataset_processed_overwrite.csv')
+
+remove_spurious_pairs(DATASET_PATH, SAVE_PATH)
