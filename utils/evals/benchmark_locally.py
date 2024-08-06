fb266d2556d7c7269e95e9f9a9706c11ef6d8fe3
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2024-08-06 01:39:50 +0100

    Updated local script

--- utils/evals/benchmark_locally.py
+++ utils/evals/benchmark_locally.py
@@ -4,40 +4,22 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 def benchmark_locally(model_name, discharge_summary, question):
     tokeniser = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
-    
-    if model_name == 'starmpcc/Asclepius-13B':
-        prompt = f"""You are an intelligent clinical languge model.
-        Below is a snippet of patient's discharge summary and a following instruction from healthcare professional.
-        Write a response that appropriately completes the instruction.
-        The response should provide the accurate answer to the instruction, while being concise.
 
-        [Discharge Summary Begin]
-        {discharge_summary}
-        [Discharge Summary End]
+    prompt = f"""
+    You are an expert medical professional tasked 
+    with answering a clinical question to the best of your ability. You 
+    must construct your answer based on the evidence provided to you in 
+    the discharge summary.\n
 
-        [Instruction Begin]
-        {question}
-        [Instruction End] 
-        """
-    else:
-
-        prompt = f"""
-        You are an expert medical professional tasked 
-        with answering a clinical question to the best of your ability. You 
-        must construct your answer based on the evidence provided to you in 
-        the discharge summary.\n
-
-        Your task is to answer a clinical question based on the
-        following discharge summary:
-        \n{discharge_summary}\n\n
-        You should give an answer and a reason for your answer in the
-        following format:
-        Answer: [your answer]\n\n
-        Question: {question}\n\n
-        Answer:
-        """
-
-
+    Your task is to answer a clinical question based on the
+    following discharge summary:
+    \n{discharge_summary}\n\n
+    You should give an answer and a reason for your answer in the
+    following format:
+    Answer: [your answer]\n\n
+    Question: {question}\n\n
+    Answer:
+    """
 
     tokenised_prompt = tokeniser(prompt, return_tensors="pt")
     generation = model.generate(tokenised_prompt["input_ids"], max_length=99999)
